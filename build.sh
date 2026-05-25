9aa5a6f7130eb59595c0fa496b2568bae0b20018
Byte Colony <goenka.ekansh@gmail.com>
2026-05-25 18:21:52 +0800

    fix: use npm install for esbuild in build.sh

--- build.sh
+++ build.sh
@@ -7,23 +7,23 @@ ESBUILD_VERSION=0.25.4
 curl -L "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" | tar -xz
 
 # Download esbuild for asset minification
-curl -fsSL "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-${ESBUILD_VERSION}.tgz" | tar -xz --strip-components=1 package/bin/esbuild
-chmod +x esbuild
+npm install --no-save esbuild@${ESBUILD_VERSION}
+ESBUILD=./node_modules/.bin/esbuild
 
 # Build Hugo site
 ./hugo --gc --minify --baseURL "https://${VERCEL_PROJECT_PRODUCTION_URL}"
 
 # Minify JS
 for f in public/js/*.js; do
-  ./esbuild "$f" --minify --outfile="$f" --allow-overwrite
+  $ESBUILD "$f" --minify --outfile="$f" --allow-overwrite
 done
 
 # Minify CSS
 for f in public/css/*.css; do
-  ./esbuild "$f" --minify --outfile="$f" --allow-overwrite --loader=css
+  $ESBUILD "$f" --minify --outfile="$f" --allow-overwrite --loader=css
 done
 
 # Minify work.css
 if [ -f public/work.css ]; then
-  ./esbuild public/work.css --minify --outfile=public/work.css --allow-overwrite --loader=css
+  $ESBUILD public/work.css --minify --outfile=public/work.css --allow-overwrite --loader=css
 fi
